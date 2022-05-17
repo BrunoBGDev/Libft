30a4c386faa4686a63404f3014e8a12036478d8c
BrunoBGDev <bruno.braga.design@gmail.com>
2022-05-17 12:08:57 -0300

    Update 0.0.4

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,39 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/11 08:35:22 by bbraga            #+#    #+#             */
-/*   Updated: 2022/05/17 10:27:27 by bbraga           ###   ########.fr       */
+/*   Created: 2022/05/17 10:31:41 by bbraga            #+#    #+#             */
+/*   Updated: 2022/05/17 12:05:23 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	count;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+	size_t				count;
 
+	str1 = (const unsigned char *) s1;
+	str2 = (const unsigned char *) s2;
 	count = 0;
-	if (n == 0)
-		return (0);
-	while (count < n - 1 && s1[count] != '\0'
-		&& s2[count] != '\0' && s1[count] == s2[count])
-		count++;
-	return ((unsigned char)s1[count] - (unsigned char)s2[count]);
+	while (count < n)
+	{
+		if (str1[count] != str2[count])
+			return (str1[count] - str2[count]);
+			count++;
+	}
+	return (0);
 }
 
 /*#include <stdio.h>
 
 int	main(void)
 {
-	char	test1[20] = "coom";
-	char	test2[20] = "cpSTE";
+	char	test1[] = "5555444";
+	char	test2[] = "5556";
 	int		func = 0;
-	
-	func = ft_strncmp(test1, test2, 4);
 
+	func = ft_memcmp(test1, test2, 5);
 	printf("%d\n", func);
+	return (0);
 }*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 09:47:56 by bbraga            #+#    #+#             */
-/*   Updated: 2022/05/16 14:57:04 by bbraga           ###   ########.fr       */
+/*   Updated: 2022/05/17 12:08:08 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 
 #include <stdlib.h>
 
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strncmp(const char *s1,const char *s2,size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
@@ -32,7 +34,9 @@ char	*ft_strchr(const char *str, int c);
 void	*ft_memset(void *str, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t len);
-size_t	ft_strlen(char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst,const char *src, size_t dstsize);
+size_t	ft_strlen(const char *str);
 
 #endif
 
