babb49f08934f419a20a118de46946a47ce91a5f
BrunoBGDev <bruno.braga.design@gamil.com>
2022-06-03 23:19:12 -0300

    Update 1.0.0

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,34 +6,75 @@
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 15:21:36 by bbraga            #+#    #+#             */
-/*   Updated: 2022/06/03 15:15:13 by bbraga           ###   ########.fr       */
+/*   Updated: 2022/06/03 16:51:13 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_estim(long n)
+{
+	size_t	estim;
+	int		isneg;
+
+	estim = 0;
+	isneg = 0;
+	if (n < 0)
+	{
+		estim++;
+		isneg++;
+		n = -n;
+	}
+	while (n >= 1)
+	{
+		estim++;
+		n /= 10;
+	}
+	return (estim);
+}
+
+static char	*ft_gen(char *rtn, long nbr, int len, int isneg)
+{
+		rtn = ft_strdup("0");
+	if (nbr != 0)
+		rtn = malloc(sizeof(char) * (len + 1));
+	else
+		return (rtn);
+	if (!rtn)
+		return (0);
+	isneg = 0;
+	if (nbr < 0)
+	{
+		isneg++;
+		nbr = -nbr;
+	}
+	rtn[len] = '\0';
+	while (--len)
+	{
+		rtn[len] = (nbr % 10) + '0';
+		nbr /= 10;
+	}
+	if (isneg == 1)
+		rtn[0] = '-';
+	else
+		rtn[0] = (nbr % 10) + '0';
+	return (rtn);
+}
+
 char	*ft_itoa(int n)
 {
-	size_t	len;
-	char	*str;
+	int		len;
+	char	*rtn;
+	long	nbr;
+	int		isneg;
 
-	len = 0;
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!str)
-		return (NULL);
-	if (n < 0)
-		n = -n;
-	while (n > 0)
-	{
-		*(str + len) = n % 10 + '\0';
-		n /= 10;
-		len++;
-	}
-	if (len == 0 && str[1] == '\0')
-		*(str + len) = '\0';
-	if (len == 0 && str[1] != '\0')
-		*(str + len) = '-';
-	return (str);
+	nbr = n;
+	len = ft_estim(nbr);
+	rtn = ft_gen(rtn, nbr, len, isneg)
+	isneg = 0;
+	if (!rtn)
+		return (0);
+	return (rtn);
 }
 
 /*#include <stdlib.h>
